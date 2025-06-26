4cddba081cb018729d5925b48f5598e60afe626c
Sean Collins <sean.collins@outlook.com>
2025-06-25 20:40:00 -0700

    Add additional employee metadata

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -2,6 +2,14 @@ export interface Employee {
   id: number;
   name: string;
   title: string;
+  email: string;
+  address: string;
+  phone: string;
+  organization: string;
+  department: string;
+  office: string;
+  status: string;
+  pay: string;
 }
 
 export let employees: Employee[] = [];
