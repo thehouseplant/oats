cd7734f3c21949db5a4cd3bf310d995c54ee1b75
Sean Collins <sean.collins@outlook.com>
2025-06-25 19:02:46 -0700

    Add basic routes and Tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router';
+import Navbar from './components/Navbar';
+import EmployeesPage from './pages/Employees';
+import AssetsPage from './pages/Assets';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen bg-gray-900 text-gray-200">
+      <Navbar />
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Routes>
+            <Route path="/" element={<EmployeesPage />} />
+            <Route path="/assets" element={<AssetsPage />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
