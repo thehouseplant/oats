5b7da9a2901f80ae7424b2b3256a48a40c410697
Sean Collins <sean.collins@outlook.com>
2025-06-29 09:01:06 -0700

    Update shadcn/ui dark mode functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
 import { Routes, Route } from 'react-router';
+import { ThemeProvider } from './components/theme-provider';
 import Navbar from './components/Navbar';
 import EmployeesPage from './pages/Employees';
 import AssetsPage from './pages/Assets';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-200">
-      <Navbar />
-      <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <Routes>
-            <Route path="/" element={<EmployeesPage />} />
-            <Route path="/assets" element={<AssetsPage />} />
-          </Routes>
-        </div>
-      </main>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen">
+        <Navbar />
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <Routes>
+              <Route path="/" element={<EmployeesPage />} />
+              <Route path="/assets" element={<AssetsPage />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
